e3b4b243efc084f5b0be4c53e41150b59b0d42f7
Takacs Krisztian <takacskrisz1024@gmail.comn>
2024-02-06 18:31:23 +0100

    changes

--- webshop/src/Components/Menu/js/menu.js
+++ webshop/src/Components/Menu/js/menu.js
@@ -9,7 +9,7 @@ function Menu(){
                 <div style={{width:'20%'}} ><span className="account" >Bejelentkezés</span></div>
             </div>
             <div>
-                <img style={{width:'100%'}} src=" https://www.shutterstock.com/shutterstock/photos/1822704041/display_1500/stock-photo-close-up-of-cropped-hand-pointing-at-webshop-inscription-online-shopping-concept-1822704041.jpg"/>
+                <img className="headerimg" src=" https://www.shutterstock.com/shutterstock/photos/1822704041/display_1500/stock-photo-close-up-of-cropped-hand-pointing-at-webshop-inscription-online-shopping-concept-1822704041.jpg"/>
             </div>
             <div></div>
         </div>
