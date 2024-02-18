3e644105c87fea1e7409793af1c14cf5443523ae
Takacs Krisztian <takacskrisz1024@gmail.comn>
2024-02-18 16:58:45 +0100

    gyerekholmik

--- webshop/src/Components/Header/js/Header.js
+++ webshop/src/Components/Header/js/Header.js
@@ -3,7 +3,7 @@ function Header(){
 
     return(
         <div className="header">
-                <img className="headerimg" src=" https://www.shutterstock.com/shutterstock/photos/1822704041/display_1500/stock-photo-close-up-of-cropped-hand-pointing-at-webshop-inscription-online-shopping-concept-1822704041.jpg"/>
+                <img className="headerimg" src=" https://st3.depositphotos.com/8543466/17138/i/1600/depositphotos_171381414-stock-photo-kids-toys-background-with-teddy.jpg"/>
         </div>
     )
 }
